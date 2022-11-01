7696abac3e8402c6f332bcf620412572113ad095
AndreiTotoro <AndreiTotoro@gmail.com>
2022-11-01 09:39:19 +0200

    Add help banner

--- app/Main.js
+++ app/Main.js
@@ -1,10 +1,23 @@
 'use client';
 import React from 'react';
+import { FiX } from 'react-icons/fi';
 
 export default function Main({ currentLayer }) {
 	return (
-		<div className="h-screen bg-midnight pl-4 text-white flex flex-col gap-7">
+		<div className="h-screen bg-midnight pl-4 text-white flex flex-col pr-4 gap-7">
 			<p className="text-2xl pt-5">You have selected: {currentLayer}</p>
+			<div className="bg-green-400 rounded-md p-2 flex justify-between">
+				<p className="text-green-700">
+					Not sure where to start? Check out our intro video{' '}
+					<a className="text-blue-500 underline" href="">
+						here
+					</a>
+					.
+				</p>
+				<button>
+					<FiX />
+				</button>
+			</div>
 			<p>Upload your images below: </p>
 		</div>
 	);
