8bd624a4fb9ad602b6e2d780594d2ea638ec396f
AndreiTotoro <AndreiTotoro@gmail.com>
2022-11-01 09:25:19 +0200

    Unified styles

--- app/Main.js
+++ app/Main.js
@@ -3,8 +3,9 @@ import React from 'react';
 
 export default function Main({ currentLayer }) {
 	return (
-		<div className="h-screen bg-midnight text-white text-2xl p-4">
-			{currentLayer}
+		<div className="h-screen bg-midnight pl-4 text-white flex flex-col gap-7">
+			<p className="text-2xl pt-5">You have selected: {currentLayer}</p>
+			<p>Upload your images below: </p>
 		</div>
 	);
 }
--- app/Settings.js
+++ app/Settings.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Settings() {
 	return (
-		<div className="h-screen text-white text-2xl p-4 bg-void">Settings</div>
+		<div className="h-screen text-white text-2xl pl-4 pt-5 bg-void">
+			Settings
+		</div>
 	);
 }
