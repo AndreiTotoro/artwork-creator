bb9954c0b1d5bb14f6e82b6fc1e71db03cb63a51
AndreiTotoro <AndreiTotoro@gmail.com>
2022-11-01 22:17:23 +0200

    Display items in grid

--- app/Main.js
+++ app/Main.js
@@ -27,19 +27,25 @@ export default function Main({ currentLayer, layerData, setLayerData }) {
 				setLayerData={setLayerData}
 				currentLayer={currentLayer}
 			/>
-
-			<div>
-				{layerData.map((layer, index) => {
+			<p className="text-2xl pt-5">Preview uploaded images: </p>
+			<div className="flex flex-wrap bg-void p-4 rounded-md">
+				{layerData.map((layer) => {
 					if (layer.images.length != 0 && layer.name == currentLayer) {
-						return layer.images.map((img) => {
+						return layer.images.map((img, index) => {
 							return (
-								<Image
+								<div
 									key={index}
-									src={img}
-									alt={'nothing'}
-									width={100}
-									height={100}
-								/>
+									className="bg-slate-600 mt-4 rounded-md mx-4 flex flex-col items-center content-center justify-center"
+								>
+									<Image
+										src={img}
+										alt={'nothing'}
+										width={200}
+										height={200}
+										className="basis-1/6 m-4"
+									/>
+									<p>{index + 1}</p>
+								</div>
 							);
 						});
 					}
