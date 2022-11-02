c42b5ad284617047588f566acf3c3ffe21c44ff7
AndreiTotoro <AndreiTotoro@gmail.com>
2022-11-02 08:42:57 +0200

    Move preview images to its own component

--- app/Main.js
+++ app/Main.js
@@ -1,7 +1,7 @@
 'use client';
-import Image from 'next/image';
 import React from 'react';
 import { FiX } from 'react-icons/fi';
+import PreviewImages from '../components/PreviewImages';
 import Upload from '../components/Upload';
 
 export default function Main({ currentLayer, layerData, setLayerData }) {
@@ -27,30 +27,7 @@ export default function Main({ currentLayer, layerData, setLayerData }) {
 				setLayerData={setLayerData}
 				currentLayer={currentLayer}
 			/>
-			<p className="text-2xl pt-5">Preview uploaded images: </p>
-			<div className="flex flex-wrap bg-void p-4 rounded-md">
-				{layerData.map((layer) => {
-					if (layer.images.length != 0 && layer.name == currentLayer) {
-						return layer.images.map((img, index) => {
-							return (
-								<div
-									key={index}
-									className="bg-slate-600 mt-4 rounded-md mx-4 flex flex-col items-center content-center justify-center"
-								>
-									<Image
-										src={img}
-										alt={'nothing'}
-										width={200}
-										height={200}
-										className="basis-1/6 m-4"
-									/>
-									<p>{index + 1}</p>
-								</div>
-							);
-						});
-					}
-				})}
-			</div>
+			<PreviewImages layerData={layerData} currentLayer={currentLayer} />
 		</div>
 	);
 }
